f59204c027359ba6e0c4226493975acf4d686ee1
Evgeniy Gribkov <grandcazino@gmail.com>
2020-02-19 16:52:00 +0200

    model update

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+# Поскольку views.py и models.py находятся в одной директории, мы можем использовать точку . и имя файла (без расширения .py)
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
